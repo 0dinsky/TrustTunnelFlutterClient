6cd84b837e308d2ada4c95ec046298a4b7f784ee
0dinsky <alex.v.ledenev234@gmail.com>
2026-05-20 08:06:35 +0300

    Fix build

--- plugins/vpn_plugin/android/src/main/kotlin/com/adguard/trusttunnel/vpn_plugin/SpeedNotificationManager.kt
+++ plugins/vpn_plugin/android/src/main/kotlin/com/adguard/trusttunnel/vpn_plugin/SpeedNotificationManager.kt
@@ -22,7 +22,7 @@ class SpeedNotificationManager(private val context: Context) {
 
     companion object {
         internal const val CHANNEL_ID      = "tt_speed_channel"
-        private  const val NOTIFICATION_ID  = 9001
+        internal const val NOTIFICATION_ID  = 9001
         private const val ACTION_STOP      = "com.adguard.trusttunnel.STOP_VPN"
         private const val UPDATE_INTERVAL  = 1000L // 1 секунда
     }
