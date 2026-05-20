2e45969caf9e4e13be3b50226c361efd25b65a7e
0dinsky <alex.v.ledenev234@gmail.com>
2026-05-20 08:18:53 +0300

    Fix build

--- plugins/vpn_plugin/android/src/main/kotlin/com/adguard/trusttunnel/vpn_plugin/SpeedNotificationManager.kt
+++ plugins/vpn_plugin/android/src/main/kotlin/com/adguard/trusttunnel/vpn_plugin/SpeedNotificationManager.kt
@@ -109,7 +109,7 @@ class SpeedNotificationManager(private val context: Context) {
     private fun buildNotification(speedText: String): Notification {
         val stopIntent = PendingIntent.getBroadcast(
             context, 0,
-            Intent(ACTION_STOP),
+            Intent(ACTION_STOP).setPackage(context.packageName),
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
 
